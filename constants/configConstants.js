b7c210370e6da0a239aa9beb41804a4cfa983a74
Daniel <myworktime002@gmail.com>
2019-05-21 17:13:24 +0530

    - Update Db Connection

--- constants/configConstants.js
+++ constants/configConstants.js
@@ -2,9 +2,4 @@ const ENVIRONMENT = require('./environment');
 
 exports.CONFIG = {
   nodeEnv: ENVIRONMENT.ENV,
-  dbName: ENVIRONMENT.DBNAME,
-  dbHost: ENVIRONMENT.DBHOST,
-  dbPort: ENVIRONMENT.DBPORT,
-  dbUserName: ENVIRONMENT.DBUSERNAME,
-  dbPassword: ENVIRONMENT.DBPASSWORD,
 };
