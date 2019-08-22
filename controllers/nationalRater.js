eb21e7514c6dd6ac52964ad5f43a21b2415dc106
Daniel <myworktime002@gmail.com>
2019-08-22 12:25:21 -0700

    Updated; ;

--- controllers/nationalRater.js
+++ controllers/nationalRater.js
@@ -57,10 +57,16 @@ module.exports = {
         await vehiclesStep();
         await underWritingStep();
       } else if (params.stepName === 'namedInsured') {
-        await namedInsuredStep();
-        await page.waitFor(1000);
-        quoteId = await page.$eval('#ctl00_lblHeaderPageTitleTop', e => e.innerText);
-        await exitSuccess('Named Insured');
+        try {
+          await namedInsuredStep();
+          await page.waitFor(1000);
+          // quoteId = await page.$eval('#ctl00_lblHeaderPageTitleTop', e => e.innerText);
+          console.log('QUOTE ID: ', quoteId);
+          await page.waitFor(1000);
+          await exitSuccess('Named Insured');
+        } catch (error) {
+          await exitFail(error, 'namedInsured');
+        }
       } else if (params.stepName === 'drivers' && raterStore) {
         await driversStep();
         if (params.sendSummary && params.sendSummary === 'true') {
@@ -70,11 +76,7 @@ module.exports = {
         }
       } else if (params.stepName === 'vehicles' && raterStore) {
         await vehiclesStep();
-        if (params.sendSummary && params.sendSummary === 'true') {
-          await underWritingStep();
-        } else {
-          await exitSuccess('Vehicles');
-        }
+        await underWritingStep();
       } else if (params.stepName === 'summary' && raterStore) {
         await underWritingStep();
       }
@@ -358,9 +360,10 @@ module.exports = {
           req.session.data = {
             title: `Successfully finished National ${step} Step`,
             status: true,
-            quoteIds: raterStore.quoteId || quoteId,
+            quoteId: raterStore && raterStore.quoteId ? raterStore.quoteId : quoteId,
             stepResult,
           };
+          console.log(req.session.data);
           browser.close();
           return next();
         } catch (error) {
