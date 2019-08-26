5e83ca4d3c37b3c0f28a42a525b79eb920780c59
Daniel <myworktime002@gmail.com>
2019-08-26 18:45:54 +0530

    Redis connection update

--- constants/configConstants.js
+++ constants/configConstants.js
@@ -4,5 +4,5 @@ exports.CONFIG = {
   nodeEnv: process.env.ENV,
   port: process.env.PORT || '4000',
   dbUrl: process.env.DATABASE_URL,
-  redisUrl: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+  redisUrl: process.env.REDIS_URL,
 };
