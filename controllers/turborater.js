7310475619c45da8bed0b9bd08b0cb8f4947c7d5
Dilip1978 <dmishra@mobivana.com>
2019-09-19 04:42:25 -0700

    code updated

--- controllers/turborater.js
+++ controllers/turborater.js
@@ -16,6 +16,7 @@ module.exports = {
 
         const homeXml_head = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const homeXml_body = homeXml_head.concat(homeXmlData, '</InsuranceRequest>');
+        const home_req = `accountnumber=${appConstant.ACC_NUM}&leadid=2354&data=${homeXml_body}`
 
         const home_options = {
           method: 'POST',
@@ -24,7 +25,7 @@ module.exports = {
           {
             'Content-Type': 'application/x-www-form-urlencoded',
           },
-          body: homeXml_body,
+          body: home_req,
         };
 
         const homeResponse = await request(home_options);
@@ -36,11 +37,10 @@ module.exports = {
         }
 
         const autoData = req.body.autoData;
-
         const autoXmlData = jsonxml(autoData);
-
         const autoXml_head = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const autoXml_body = autoXml_head.concat(autoXmlData, '</InsuranceRequest>');
+        const auto_req = `accountnumber=${appConstant.ACC_NUM}&leadid=2354&data=${autoXml_body}`
 
         const auto_options = {
           method: 'POST',
@@ -49,7 +49,7 @@ module.exports = {
           {
             'Content-Type': 'application/x-www-form-urlencoded',
           },
-          body: autoXml_body,
+          body: auto_req,
         };
 
         const autoResponse = await request(auto_options);
@@ -70,7 +70,7 @@ module.exports = {
         const xml_head = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const xml_body = xml_head.concat(xmlData, '</InsuranceRequest>');
         // const xml__body = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest><ContactInfo><FirstName>Sample</FirstName><LastName>Sample</LastName><Address>4444 some st</Address><ZipCode>92374</ZipCode><City>Redlands</City><County>San Bernardino</County><State>CA</State><PhoneDay>111-111-1111</PhoneDay><PhoneEve>222-222-2222</PhoneEve><PhoneCell>333-333-3333</PhoneCell><Email>sample@email.com</Email></ContactInfo><AutoInsurance><Vehicles><Vehicle><Id>1</Id><Vin>1HGCG669*YA******</Vin><Year>2000</Year><Make>HONDA</Make><Model>ACCORD</Model><SubModel>SE</SubModel><Owner>Owned</Owner><Garage>Locked</Garage><PrimaryUse>Commute Varies</PrimaryUse><OneWay>40</OneWay><AnnualMileage>10000</AnnualMileage></Vehicle></Vehicles><Drivers><Driver><Id>1</Id><FirstName>Paige</FirstName><LastName>Johnson</LastName><Gender>Female</Gender><Marital>Single</Marital><DOB>1980-07-31</DOB><Relation>Self</Relation><DLicenseState>FL</DLicenseState><DLicenseStatus>Active</DLicenseStatus><LicensedAge>44</LicensedAge><VehicleId>1</VehicleId><Filing>0</Filing><Education>Masters Degree</Education><Occupation>Student</Occupation><Incidents><Ticket><DriverId>1</DriverId><Date>N/A</Date><Description></Description></Ticket><MajorViolation><DriverId>1</DriverId><Date>N/A</Date></MajorViolation><Accident><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Accident><Claim><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Claim></Incidents></Driver></Drivers><ApplicantInfo><Credit>Good</Credit><IsMilitary>false</IsMilitary></ApplicantInfo><CurrentInsurance><CurrentlyInsured>false</CurrentlyInsured></CurrentInsurance></AutoInsurance></InsuranceRequest>'
-        const encodedData = `accountnumber=${appConstant.ACC_NUM}&leadid=2354&data=${xml_body}`
+        const body_req = `accountnumber=${appConstant.ACC_NUM}&leadid=2354&data=${xml_body}`
         const options = {
           method: 'POST',
           url: configConstant.nodeEnv === 'production' ? appConstant.UPLOAD_PATH : appConstant.UPLOAD_PATH_DEV,
@@ -78,9 +78,8 @@ module.exports = {
           {  
             'Content-Type': 'application/x-www-form-urlencoded',
           },
-          body: encodedData,
+          body: body_req,
         };
-        console.log('xml_body', encodedData);
         const response = await request(options);
         console.log('response', response);
         let newResponse;
