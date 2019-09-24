46aef1c6053c990204cf70e4be5bfb9f156b6dcc
Dilip1978 <dmishra@mobivana.com>
2019-09-24 08:51:38 -0700

    console removed

--- controllers/turborater.js
+++ controllers/turborater.js
@@ -63,7 +63,6 @@ module.exports = {
         const xmlData = jsonxml(req.body.data);
         const xml_head = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const xml_body = xml_head.concat(xmlData, '</InsuranceRequest>');
-        // console.log('xml_body_data', xml_body);
         // const xml_body = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest><ContactInfo><FirstName>Sample</FirstName><LastName>Sample</LastName><Address>4444 some st</Address><ZipCode>92374</ZipCode><City>Redlands</City><County>San Bernardino</County><State>CA</State><PhoneDay>111-111-1111</PhoneDay><PhoneEve>222-222-2222</PhoneEve><PhoneCell>333-333-3333</PhoneCell><Email>sample@email.com</Email></ContactInfo><AutoInsurance><Vehicles><Vehicle><Id>1</Id><Vin>1HGCG669*YA******</Vin><Year>2000</Year><Make>HONDA</Make><Model>ACCORD</Model><SubModel>SE</SubModel><Owner>Owned</Owner><Garage>Locked</Garage><PrimaryUse>Commute Varies</PrimaryUse><OneWay>40</OneWay><AnnualMileage>10000</AnnualMileage></Vehicle></Vehicles><Drivers><Driver><Id>1</Id><FirstName>Paige</FirstName><LastName>Johnson</LastName><Gender>Female</Gender><Marital>Single</Marital><DOB>1980-07-31</DOB><Relation>Self</Relation><DLicenseState>FL</DLicenseState><DLicenseStatus>Active</DLicenseStatus><LicensedAge>44</LicensedAge><VehicleId>1</VehicleId><Filing>0</Filing><Education>Masters Degree</Education><Occupation>Student</Occupation><Incidents><Ticket><DriverId>1</DriverId><Date>N/A</Date><Description></Description></Ticket><MajorViolation><DriverId>1</DriverId><Date>N/A</Date></MajorViolation><Accident><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Accident><Claim><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Claim></Incidents></Driver></Drivers><ApplicantInfo><Credit>Good</Credit><IsMilitary>false</IsMilitary></ApplicantInfo><CurrentInsurance><CurrentlyInsured>false</CurrentlyInsured></CurrentInsurance></AutoInsurance></InsuranceRequest>'
         const body_req = `accountnumber=${username}&leadid=${req.body.leadId}&data=${xml_body}`
         const options = {
@@ -77,7 +76,6 @@ module.exports = {
         };
         const response = await request(options);
         const jsonresponse = convert.xml2json(response, {compact: true, spaces: 4});
-        console.log('response', jsonresponse);
         // if (jsonresponse.Result._text !== 'OK' || jsonresponse.ErrorCode._text !== 0) {
         //   throw new Error(jsonresponse.Result._text);
         // }
