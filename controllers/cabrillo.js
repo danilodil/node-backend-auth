a5f7d4c6d65644e6fa96225e092013b739b41794
Dilip1978 <dmishra@mobivana.com>
2019-09-26 02:34:36 -0700

    cabrillo changes

--- controllers/cabrillo.js
+++ controllers/cabrillo.js
@@ -15,21 +15,20 @@ const self = module.exports = {
         method: 'post',
         body: data,
         json: true,
-        url: "https://test.cabgen.com/agents/login",
+        url: "https://test.cabgen.com/agents/token",
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
       };
 
       if (configConstant.ENV === 'local') {
-        options.url = "https://test.cabgen.com/agents/login";
+        options.url = "https://test.cabgen.com/agents/token";
       }
 
       const response = await request(options);
-      console.log('response', response);
       return response;
     } catch (error) {
-      console.log('Error at Cabrillo : ', error.stack);
+      console.log('Error at Cabrillo token : ', error.stack);
       return next(Boom.badRequest('Failed to retrieved Cabrillo Token.'));
     }
   },
@@ -37,49 +36,40 @@ const self = module.exports = {
   createContact: async (req, res, next) => {
     try {
       const token = await self.getToken(req.body.decoded_vendor.username, req.body.decoded_vendor.password);
-      console.log('token ######################', JSON.stringify(token));
       const quoteData = jsonxml(req.body.data);
 
       const xmlHead = '<?xml version="1.0" encoding="utf-8"?>';
       const xmlBody = xmlHead.concat(quoteData);
-
+      console.log('xml request', xmlBody);
       const encodedData = base64.encode(xmlBody);
 
         const data = {
           Quote: encodedData,
           Vendor: 'XO',
-         // ApiKey: '05a156a5ffc24a388352b90b8f339f54',
+          ApiKey: '05a156a5ffc24a388352b90b8f339f54',
         };
-
-        
-
         const options = {
           method: 'post',
           body: data,
           json: true,
-          url: "https://test.cabgen.com/agents/token",
+          url: "https://test.cabgen.com/agents/bridge/rate",
           headers: {
             Authorization: `Bearer ${token.access_token}`,
             'Content-Type': 'application/json',
           },
         };
-        
         if (configConstant.ENV === 'local') {
           data.ApiKey = '05a156a5ffc24a388352b90b8f339f54';
-          options.url = "https://test.cabgen.com/agents/token";
+          options.url = "https://test.cabgen.com/agents/bridge/rate";
         }
-
-        return request(options, (error, response, body) => {
-          if (error) {
-            return next(Boom.badRequest('Error getting token'));
-          }
-          console.log('response #####################', JSON.stringify(response));
-          console.log('body ########################', JSON.stringify(body));
-          return res.status(200)
-            .json(response);
-        });
+        const response = await request(options);
+        req.session.data = {
+          title: 'Contact created successfully',
+          body: response,
+        };
+        return next();
     } catch (error) {
-      console.log(error);
+      console.log('ERROR ###', error)
       return next(Boom.badRequest('Error creating contact'));
     }
   },
