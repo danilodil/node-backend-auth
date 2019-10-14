a4dc87f451cdd31fa2d9b63704b3c9aa18cd5ec4
Daniel <myworktime002@gmail.com>
2019-10-14 15:15:51 +0530

    Feat: appulate integration done

---
+++ controllers/appulate.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+
+const request = require('request-promise');
+const Boom = require('boom');
+const appConstant = require('../constants/appConstant').appulate;
+
+module.exports = {
+  createContact: async (req, res, next) => {
+    try {
+      const { username, password } = req.body.decoded_vendor;
+
+      if (!req.body.zipFilebuffer) {
+        return next(Boom.badData('Invalid Data'));
+      }
+      const options = {
+        method: 'POST',
+        url: appConstant.UPLOAD_PATH,
+        json: true,
+        body: {
+          Credentials: {
+            Email: username,
+            Password: password,
+          },
+          Content: req.body.zipFilebuffer,
+        },
+      };
+      const response = await request(options);
+      req.session.data = {
+        title: 'Contact created successfully',
+        response,
+      };
+      return next();
+    } catch (error) {
+      console.log('error', error);
+      return next(Boom.badRequest('Failed to contact'));
+    }
+  },
+};
---
+++ routes/appulate.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const appulateController = require('../controllers/appulate');
+
+const router = express.Router();
+
+router.put('/upsert', asyncHandler(async (req, res, next) => {
+  await appulateController.createContact(req, res, next);
+}));
+
+module.exports = router;
