2a6f0a135f7a46649d2b90ab899e83a83b39c322
Daniel <myworktime002@gmail.com>
2019-10-27 14:24:05 -0700

    Updaed

--- controllers/turborater.js
+++ controllers/turborater.js
@@ -18,7 +18,7 @@ module.exports = {
 
         const homeXmlHead = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const homeXmlBody = homeXmlHead.concat(homeXmlData, '</InsuranceRequest>');
-        const homeReq = `accountnumber=${username}&leadid=${req.body.LeadId}&data=${homeXmlBody}`;
+        const homeReq = `accountnumber=${username}&leadid=${req.body.leadId}&data=${homeXmlBody}`;
 
         const homeOptions = {
           method: 'POST',
@@ -35,7 +35,7 @@ module.exports = {
         const autoXmlData = jsonxml(autoData);
         const autoXmlHead = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const autoXmlBody = autoXmlHead.concat(autoXmlData, '</InsuranceRequest>');
-        const autoReq = `accountnumber=${username}&leadid=${req.body.LeadId}&data=${autoXmlBody}`;
+        const autoReq = `accountnumber=${username}&leadid=${req.body.leadId}&data=${autoXmlBody}`;
 
         const autoOptions = {
           method: 'POST',
@@ -71,7 +71,7 @@ module.exports = {
         const xmlHead = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest>';
         const xmlBody = xmlHead.concat(xmlData, '</InsuranceRequest>');
         // const xmlBody = '<?xml version="1.0" encoding="utf-8"?><InsuranceRequest><ContactInfo><FirstName>Sample</FirstName><LastName>Sample</LastName><Address>4444 some st</Address><ZipCode>92374</ZipCode><City>Redlands</City><County>San Bernardino</County><State>CA</State><PhoneDay>111-111-1111</PhoneDay><PhoneEve>222-222-2222</PhoneEve><PhoneCell>333-333-3333</PhoneCell><Email>sample@email.com</Email></ContactInfo><AutoInsurance><Vehicles><Vehicle><Id>1</Id><Vin>1HGCG669*YA******</Vin><Year>2000</Year><Make>HONDA</Make><Model>ACCORD</Model><SubModel>SE</SubModel><Owner>Owned</Owner><Garage>Locked</Garage><PrimaryUse>Commute Varies</PrimaryUse><OneWay>40</OneWay><AnnualMileage>10000</AnnualMileage></Vehicle></Vehicles><Drivers><Driver><Id>1</Id><FirstName>Paige</FirstName><LastName>Johnson</LastName><Gender>Female</Gender><Marital>Single</Marital><DOB>1980-07-31</DOB><Relation>Self</Relation><DLicenseState>FL</DLicenseState><DLicenseStatus>Active</DLicenseStatus><LicensedAge>44</LicensedAge><VehicleId>1</VehicleId><Filing>0</Filing><Education>Masters Degree</Education><Occupation>Student</Occupation><Incidents><Ticket><DriverId>1</DriverId><Date>N/A</Date><Description></Description></Ticket><MajorViolation><DriverId>1</DriverId><Date>N/A</Date></MajorViolation><Accident><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Accident><Claim><DriverId>1</DriverId><Date>N/A</Date><Description></Description><PaidAmount></PaidAmount></Claim></Incidents></Driver></Drivers><ApplicantInfo><Credit>Good</Credit><IsMilitary>false</IsMilitary></ApplicantInfo><CurrentInsurance><CurrentlyInsured>false</CurrentlyInsured></CurrentInsurance></AutoInsurance></InsuranceRequest>';
-        const bodyReq = `accountnumber=${username}&leadid=${req.body.LeadId}&data=${xmlBody}`;
+        const bodyReq = `accountnumber=${username}&leadid=${req.body.leadId}&data=${xmlBody}`;
         console.log(bodyReq);
         const options = {
           method: 'POST',
