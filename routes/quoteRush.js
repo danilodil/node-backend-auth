3adc331a1c1d33a9e16b1a57d61f1830fe95de51
Daniel <myworktime002@gmail.com>
2019-12-12 14:36:49 +0530

    Fix: quoterush integration updates

--- routes/quoteRush.js
+++ routes/quoteRush.js
@@ -5,7 +5,7 @@ const quoteRushController = require('../controllers/quoteRush');
 
 const router = express.Router();
 
-router.put('/upsert/:type/:clientId', async (req, res, next) => {
+router.put('/upsert/:clientId', async (req, res, next) => {
   await quoteRushController.createContact(req, res, next);
 });
 
