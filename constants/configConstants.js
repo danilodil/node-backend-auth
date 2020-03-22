a4788643e8f686c1146eb2caccfd6d169c921c2e
Daniel <myworktime002@gmail.com>
2020-03-21 19:06:24 -0700

    Updated for new urls

--- constants/configConstants.js
+++ constants/configConstants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 exports.CONFIG = {
-  nodeEnv: process.env.ENV,
+  nodeEnv: process.env.NODE_ENV,
   port: process.env.PORT || '4000',
   dbUrl: process.env.DATABASE_URL,
   redisUrl: process.env.REDIS_URL,
