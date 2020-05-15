6b3834a3385cc7b07b0eb5763a80da57f9a3758a
Jon Corrin <jon@xilo.io>
2020-05-15 07:36:47 -0700

    Updated for new v2 route

--- routes/ezlynx.js
+++ routes/ezlynx.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const ezlynxController = require('../controllers/ezlynx');
+const v2EzlynxController = require('../controllers/v2-ezlynx');
 
 const router = express.Router();
 
 router.put('/upsert/:type/:clientId', async (req, res, next) => {
   await ezlynxController.createContact(req, res, next);
+});
+
+router.put('/v2/upsert/:type/:clientId', async (req, res, next) => {
+  await v2EzlynxController.createApplicant(req, res, next);
 });
 
 router.put('/upsert-personal-applicant/:clientId', async (req, res, next) => {
