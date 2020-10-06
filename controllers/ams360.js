a07da203463531cd6a31ef3a38f828eeb31f1947
Daniel <myworktime002@gmail.com>
2020-10-06 10:47:41 +0530

    error message issue fixed for ams360

--- controllers/ams360.js
+++ controllers/ams360.js
@@ -44,9 +44,9 @@ const self = module.exports = {
       return next(Boom.badRequest('Error authenticating AMS360 user. Invalid token'));
     } catch (error) {
       if(error.error.includes('<faultstring>')) {
-          const errorString ='<faultstring>'.length
-          sIndex=error.error.search('<faultstring>')
-          eIndex=error.error.search('</faultstring>')
+          const errorString = '<faultstring>'.length
+          sIndex = error.error.search('<faultstring>')
+          eIndex = error.error.search('</faultstring>')
           return next(Boom.badRequest(error.error.slice(sIndex+errorString, eIndex)))
       }
       return next(Boom.badRequest('Error authenticating AMS360 user. Method failed'));
